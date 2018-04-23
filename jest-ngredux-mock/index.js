d45fe134a57a97d013f463e0f41caa4240932ab1
iGitScor <sebastien.correaud@gmail.com>
2018-04-23 10:29:31 +0200

    Realse 0.1.3

--- jest-ngredux-mock/index.js
+++ jest-ngredux-mock/index.js
@@ -1,18 +1,22 @@
-var NgReduxMock = (function() {
-  function NgReduxMock() {
-    this.configureStore = jest.fn();
-    this.configureSubStore = jest.fn();
-    this.dispatch = jest.fn();
-    this.getState = jest.fn();
-    this.provideStore = jest.fn();
-    this.replaceReducer = jest.fn();
-    this.select = jest.fn();
-    this.subscribe = jest.fn();
-  }
-  return NgReduxMock;
-})({});
+const subscription = {};
+subscription.unsubscribe = jest.fn();
 
-var ngReduxMock = new NgReduxMock();
-ngReduxMock.select.mockReturnValue(ngReduxMock);
+const observer = {};
+observer.subscribe = jest.fn().mockReturnValue(subscription);
+observer.pipe = jest.fn().mockReturnValue(observer);
 
-exports.ngReduxMock = ngReduxMock;
+var NgReduxMock = jest.fn().mockImplementation(() => {
+  var instance = {};
+  instance.configureStore = jest.fn();
+  instance.configureSubStore = jest.fn();
+  instance.dispatch = jest.fn().mockReturnValue(instance);
+  instance.getState = jest.fn();
+  instance.provideStore = jest.fn();
+  instance.replaceReducer = jest.fn();
+  instance.select = jest.fn().mockReturnValue(observer);
+  instance.subscribe = jest.fn().mockReturnValue(subscription);
+
+  return instance;
+});
+
+exports.ngReduxMock = NgReduxMock();
